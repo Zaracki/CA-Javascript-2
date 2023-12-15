04042aadc89f15cd9911604cad1925b4a8b6be25
Zaracki <119800248+Zaracki@users.noreply.github.com>
2023-12-15 14:27:32 +0100

    Single post by ID generates HTML

--- js/singlePost.mjs
+++ js/singlePost.mjs
@@ -10,20 +10,52 @@ function getIdFromUrl() {
   return null;
 }
 
+const resultsContainer = document.querySelector("#postContainer");
+
 function generateSinglePostHtml(post) {
+  const {title, body, media} = post;
+
+  const postCard = document.createElement("div");
+  postCard.className = "card mt-3";
+
+  const postContain = document.createElement("div");
+  postContain.className = "col d-flex justify-left mb-1";
+
+  const postTitle = document.createElement("h2")
+  postTitle.className = "mb-1 text-break";
+  postTitle.textContent = title;
+
+  const postBody = document.createElement("p")
+  postBody.className = "mt-0 mb-1 text-break";
+  postBody.textContent = body;
+
+  postContain.appendChild(postTitle);
+  postContain.appendChild(postBody);
   
+  const image = document.createElement("img")
+  if (media) {
+    image.src = media;
+    image.className = "user-post-img";
+    postContain.appendChild(image);
+  }
+
+  postCard.appendChild(postContain);
+
+  return postCard;
 }
 
 async function getSinglePost(id) {
   const data = await makeRequest(`${POSTS_API_URL}/${id}?_author=true`, {method: "GET"}, true);
   console.log(data)
+  return data;
 }
 
 async function generateSinglePost() {
   const postId = getIdFromUrl();
   if (postId) {
    const post = await getSinglePost(postId); 
-   generateSinglePostHtml(post);
+   const currentSinglePost = generateSinglePostHtml(post);
+   resultsContainer.appendChild(currentSinglePost); 
   }
 }
 
--- js/utils.mjs/deletePost.mjs
+++ js/utils.mjs/deletePost.mjs
@@ -2,10 +2,19 @@ import { makeRequest } from "../fetch.mjs";
 import { POSTS_API_URL } from "../constants.mjs";
 import { refreshFeed } from "../home-feed.mjs";
 
-async function deletePost (id) {
-  await makeRequest(`${POSTS_API_URL}/${id}`, { method: "DELETE"
-}, true)
-  refreshFeed();
+async function deletePost(id) {
+  // Display a confirmation dialog
+  const isConfirmed = window.confirm("Are you sure you want to delete this post?");
+
+  if (isConfirmed) {
+    try {
+      await makeRequest(`${POSTS_API_URL}/${id}`, { method: "DELETE" }, true);
+      refreshFeed();
+    } catch (error) {
+      console.error("Failed to delete post", error);
+      // Handle the error appropriately
+    }
+  } 
 }
 
 export { deletePost };
